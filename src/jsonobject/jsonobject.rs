4b074e53592feeeed43313ad4a17fa0429f94afd
OldSinner <antylifepatia@gmail.com>
2024-05-19 22:39:33 +0200

    Some cleaning

---
+++ src/jsonobject/jsonobject.rs
@@ -0,0 +1,30 @@
+pub mod jsonobject {
+    use std::collections::HashMap;
+    pub enum JsonObject {
+        Number(f32),
+        Text(String),
+        Boolean(bool),
+        Array(Vec<JsonObject>),
+        Object(HashMap<String, JsonObject>),
+    }
+
+    impl JsonObject {
+        pub fn get_string(self) -> String {
+            match self {
+                JsonObject::Text(s) => s,
+                JsonObject::Number(n) => n.to_string(),
+                JsonObject::Boolean(b) => {
+                    if b {
+                        String::from("true")
+                    } else {
+                        String::from("false")
+                    }
+                }
+                _ => panic!("Not implemented! Aaaa!"),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests;
--- src/jsonobject/mod.rs
+++ src/jsonobject/mod.rs
@@ -1,28 +1,50 @@
 pub mod jsonobject {
-    use std::collections::HashMap;
-    #[derive(Debug)]
-    pub enum JsonObject {
-        Number(f32),
-        Text(String),
-        Boolean(bool),
-        Array(Vec<JsonObject>),
-        Object(HashMap<String, JsonObject>),
+    use std::{collections::HashMap, f32, fmt::Error};
+
+    pub struct JsonObject {
+        obj: JsonEntity,
     }
 
     impl JsonObject {
-        pub fn get_value_as_string(&self) -> String {
-            match self {
-                JsonObject::Text(s) => quote(&s),
-                JsonObject::Number(n) => n.to_string(),
-                JsonObject::Boolean(b) => b.to_string(),
-                JsonObject::Array(arr) => build_from_array(&arr),
-                JsonObject::Object(obj) => build_from_obj(&obj),
-                _ => panic!("Not implemented! Aaaa!"),
+        pub fn new() -> JsonObject {
+            let map = HashMap::new();
+            let ent = JsonEntity::Object(map);
+            JsonObject { obj: ent }
+        }
+
+        pub fn add_text(&self, name: String, value: String) -> Result<String, Error> {
+            match &self.obj {
+                JsonEntity::Object(map) => {}
+                _ => panic!("Wrong type of entity!"),
             }
         }
     }
 
-    fn build_from_obj(obj: &HashMap<String, JsonObject>) -> String {
+    #[derive(Debug)]
+    enum JsonEntity {
+        Number(f32),
+        Text(String),
+        Boolean(bool),
+        Array(Vec<JsonEntity>),
+        Object(HashMap<String, JsonEntity>),
+    }
+
+    impl JsonEntity {
+        pub fn get_value_as_string(&self) -> String {
+            match self {
+                JsonEntity::Text(s) => quote(&s),
+                JsonEntity::Number(n) => n.to_string(),
+                JsonEntity::Boolean(b) => b.to_string(),
+                JsonEntity::Array(arr) => build_from_array(&arr),
+                JsonEntity::Object(obj) => build_from_obj(&obj),
+            }
+        }
+        pub fn flush_to_string(self) -> String {
+            self.get_value_as_string()
+        }
+    }
+
+    fn build_from_obj(obj: &HashMap<String, JsonEntity>) -> String {
         let mut st = String::from("{");
 
         for (key, val) in obj.iter() {
@@ -33,7 +55,7 @@ pub mod jsonobject {
         st
     }
 
-    fn build_from_array(arr: &Vec<JsonObject>) -> String {
+    fn build_from_array(arr: &Vec<JsonEntity>) -> String {
         let mut st = String::from("[");
         for i in arr {
             st.push_str((i.get_value_as_string() + ",").as_str());
@@ -46,6 +68,67 @@ pub mod jsonobject {
     fn quote(str: &String) -> String {
         format!("\"{str}\"")
     }
+
+    #[test]
+    fn JsonEntity_boolean_get_value_as_string_test() {
+        let x = JsonEntity::Boolean(false);
+        assert_eq!(x.get_value_as_string(), String::from("false"))
+    }
+
+    #[test]
+    fn JsonEntity_text_get_value_as_string_test() {
+        let x = JsonEntity::Text(String::from("test"));
+        assert_eq!(x.get_value_as_string(), String::from("\"test\""));
+        println!("{:?}", x);
+    }
+
+    #[test]
+    fn JsonEntity_intnumber_get_value_as_string_test() {
+        let x = JsonEntity::Number(10.0);
+        assert_eq!(x.get_value_as_string(), String::from("10"));
+    }
+
+    #[test]
+    fn JsonEntity_floatnumber_get_value_as_string_test() {
+        let x = JsonEntity::Number(10.1);
+        assert_eq!(x.get_value_as_string(), String::from("10.1"));
+    }
+
+    #[test]
+    fn JsonEntity_array_ofnumbes_get_value_as_string_test() {
+        let x = JsonEntity::Array(vec![
+            JsonEntity::Number(10.0),
+            JsonEntity::Number(3.1),
+            JsonEntity::Number(12.0),
+        ]);
+        assert_eq!(x.get_value_as_string(), String::from("[10,3.1,12]"));
+    }
+
+    #[test]
+    fn JsonEntity_array_get_value_as_string_test() {
+        let x = JsonEntity::Array(vec![
+            JsonEntity::Text(String::from("asd")),
+            JsonEntity::Text(String::from("asd")),
+            JsonEntity::Text(String::from("asd")),
+        ]);
+        assert_eq!(
+            x.get_value_as_string(),
+            String::from("[\"asd\",\"asd\",\"asd\"]")
+        );
+    }
+    fn get_hashpmap() -> HashMap<String, JsonEntity> {
+        let mut map = HashMap::new();
+        map.insert(String::from("BooleanValue"), JsonEntity::Boolean(false));
+        map.insert(String::from("NumericValue"), JsonEntity::Number(10.0));
+        map
+    }
+    #[test]
+    fn JsonEntity_object_get_value_as_string_test() {
+        let map = get_hashpmap();
+        let obj = JsonEntity::Object(map);
+        assert_eq!(
+            obj.get_value_as_string(),
+            String::from("{\"BooleanValue\":false,\"NumericValue\":10}")
+        );
+    }
 }
-#[cfg(test)]
-mod tests;

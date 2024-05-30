7d2b7a65471d2497bea3c65deb76761198ea1364
OldSinner <antylifepatia@gmail.com>
2024-05-30 22:45:31 +0200

    Fixed number building

--- src/json_object/json_parser.rs
+++ src/json_object/json_parser.rs
@@ -1,6 +1,8 @@
 use std::{char, str::Chars, vec};
 
 use super::json_entity::JsonEntity;
+
+static NumericChar: [char; 11] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
 #[derive(Debug)]
 pub struct JsonTokenValue {
     value_type: JsonTokenType,
@@ -23,47 +25,71 @@ pub enum JsonTokenType {
 pub fn build_token_vec(chars: &mut Chars) -> Vec<JsonTokenValue> {
     let mut vec = Vec::new();
     while let Some(c) = chars.next() {
-        if c == '{' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::BraceOpen,
-                value: c.to_string(),
-            })
-        } else if c == '}' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::BraceClose,
-                value: c.to_string(),
-            })
-        } else if c == '[' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::BracketOpen,
-                value: c.to_string(),
-            })
-        } else if c == ']' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::BracketClose,
-                value: c.to_string(),
-            })
-        } else if c == ':' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::Colon,
-                value: c.to_string(),
-            })
-        } else if c == ',' {
-            vec.push(JsonTokenValue {
-                value_type: JsonTokenType::Comma,
-                value: c.to_string(),
-            })
-        } else if c == 'f' {
-            check_if_false(chars, &mut vec);
-        } else if c == 't' {
-            check_if_true(chars, &mut vec)
-        } else if c == 'n' {
-            check_if_null(chars, &mut vec)
-        } else if c == '"' {
-            get_string_value(chars, &mut vec);
-        }
+        determinate_token(c, &mut vec, chars);
     }
     vec
+}
+
+fn determinate_token(c: char, vec: &mut Vec<JsonTokenValue>, chars: &mut Chars) {
+    if c == '{' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::BraceOpen,
+            value: c.to_string(),
+        })
+    } else if c == '}' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::BraceClose,
+            value: c.to_string(),
+        })
+    } else if c == '[' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::BracketOpen,
+            value: c.to_string(),
+        })
+    } else if c == ']' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::BracketClose,
+            value: c.to_string(),
+        })
+    } else if c == ':' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::Colon,
+            value: c.to_string(),
+        })
+    } else if c == ',' {
+        vec.push(JsonTokenValue {
+            value_type: JsonTokenType::Comma,
+            value: c.to_string(),
+        })
+    } else if c == 'f' {
+        check_if_false(chars, vec);
+    } else if c == 't' {
+        check_if_true(chars, vec)
+    } else if c == 'n' {
+        check_if_null(chars, vec)
+    } else if c == '"' {
+        get_string_value(chars, vec);
+    } else if NumericChar.contains(&c) {
+        get_number_value(chars, vec, c);
+    }
+}
+
+fn get_number_value(chars: &mut Chars, vec: &mut Vec<JsonTokenValue>, c: char) {
+    let mut value = String::new();
+    value.push(c);
+    while let Some(c_n) = chars.next() {
+        if NumericChar.contains(&c_n) {
+            value.push(c_n);
+            continue;
+        } else {
+            determinate_token(c_n, vec, chars);
+            break;
+        }
+    }
+    vec.push(JsonTokenValue {
+        value_type: JsonTokenType::Number,
+        value: value,
+    });
 }
 
 fn get_string_value(chars: &mut Chars, vec: &mut Vec<JsonTokenValue>) {
@@ -167,4 +193,25 @@ fn get_string_value_test() {
     let value = vec.first();
     assert!(value.unwrap().value == "Abdul");
     assert!(chars.count() == 0);
+}
+
+#[test]
+fn get_number_value_test() {
+    let mut str = String::from("322.1");
+    let mut chars = str.chars();
+    let mut vec = Vec::new();
+    get_number_value(&mut chars, &mut vec, '3');
+    assert!(vec.len() > 0);
+    let value = vec.first();
+    assert!(value.unwrap().value == "3322.1");
+    assert!(chars.count() == 0);
+}
+
+#[test]
+fn build_token_array_test() {
+    let mut str =
+        String::from("{\"String\":\"John123\",\"Number\":1231 , \"Object\":{},\"TrueValue\":true}");
+    let mut chars = str.chars();
+    let vec = build_token_vec(&mut chars);
+    assert!(vec.len() == 18)
 }
